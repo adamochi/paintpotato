445f0a67a08740d75bd4528801f4ce09df463692
adamochi <101391749+adamochi@users.noreply.github.com>
2022-04-07 20:34:13 +0900

    2.4

---
+++ app.js
@@ -0,0 +1,49 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
+}
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        //  ctx.closePath();
+    }
+}
+
+ function handleColorClick(event) {
+     const color = event.target.style.backgroundColor;
+     ctx.strokeStyle = color;
+ }
+
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting)
+}
+
+Array.from(colors).forEach(potato => potato.addEventListener("click", handleColorClick)
+);
